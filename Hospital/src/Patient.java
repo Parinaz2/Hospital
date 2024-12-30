63d6015fb35b71599e07c091e88aa1e6c0c3ede8
parinaz544 <parinazfirouzi544@gmail.com>
2024-12-30 13:44:41 +0330

    updated

--- Hospital/src/Patient.java
+++ Hospital/src/Patient.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Patient extends Person {
     private String bimari;
     private int roomNumber;
@@ -27,8 +32,29 @@ public class Patient extends Person {
     public void setRoomNumber(int roomNumber){
         this.roomNumber=roomNumber;
     }
+    public void addToDatabase(){
+
+        try (Connection connection = DatabaseManager.getConnection()) {
+            String query = "INSERT INTO patients (name, roomNumber, bimari, age) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, getName());
+            preparedStatement.setInt(2, roomNumber);
+            preparedStatement.setString(3, bimari);
+            preparedStatement.setInt(4, age);
+
+            int result = preparedStatement.executeUpdate();
+            if (result > 0) {
+                System.out.println("Patient added successfully!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void displayDetails() {
-        System.out.println("Patient Name: " + name + ", ID: " + id + ", Room: " + roomNumber + ", Bimari: " + bimari);
+        System.out.println("Patient Name: " + name + "\n"+
+                "ID: " + id +"\n"+
+                "Room: " + roomNumber + "\n"+
+                "Bimari: " + bimari);
     }
 }
