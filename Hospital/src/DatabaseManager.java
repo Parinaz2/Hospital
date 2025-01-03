491c9780087ffe7055a50334c8c1b0bcd8358d0f
parinaz544 <parinazfirouzi544@gmail.com>
2025-01-03 16:07:48 +0330

    updated

--- Hospital/src/DatabaseManager.java
+++ Hospital/src/DatabaseManager.java
@@ -7,7 +7,7 @@ public class DatabaseManager {
     private static final String PASSWORD = "Nit-40230112";
     public static Connection getConnection() throws SQLException {
         try {
-            // بارگذاری
+            // بارگذاریه
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
